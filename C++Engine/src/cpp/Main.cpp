69bff0d9a5a34b2d353d77de4ecbc2a8e0dd6a24
KylePearce-SoftwareDeveloper <kylepearcesoftwaredeveloper@gmail.com>
2020-09-15 16:28:05 +0100

    #2 - Iterator Data Structures

--- C++Engine/src/cpp/Main.cpp
+++ C++Engine/src/cpp/Main.cpp
@@ -23,49 +23,74 @@ void PrintVector(const Vector<Vector3>& vector)
 
 int main()
 {
-	//Using Array.
-	Array<std::string, 2> data;
-	data[0] = "Kyle";
-	data[1] = "Pearce";
+	Vector<std::string> values;
+	values.EmplaceBack("1");
+	values.EmplaceBack("2");
+	values.EmplaceBack("3");
+	values.EmplaceBack("Willy");
+	values.EmplaceBack("4");
+	values.EmplaceBack("5");
 
-	/*for (size_t i = 0; i < data.Size(); i++)
+	std::cout << "Not using iterators:\n";
+	for (int i = 0; i < values.Size(); i++)
 	{
-		std::cout << data[i] << std::endl;
-	}*/
-
-	{
-		//Using Vector.
-		Vector<Vector3> vector;
-		vector.EmplaceBack(1.0f);
-		vector.EmplaceBack(2, 3, 4);
-		vector.EmplaceBack(1, 3, 4);
-		vector.EmplaceBack();
-		PrintVector(vector);
-		vector.PopBack();
-		vector.PopBack();
-		PrintVector(vector);
-		vector.EmplaceBack(5, 3, 0);
-		vector.EmplaceBack(1, 7, 9);
-		PrintVector(vector);
-
-		vector.Clear();
-		PrintVector(vector);
-		vector.EmplaceBack(5, 3, 0);
-		vector.EmplaceBack(1, 7, 9);
-		PrintVector(vector);
+		std::cout << values[i] << std::endl;
 	}
 
-	Vector<int> intVector;
-	intVector.PushBack(5);
-	intVector.EmplaceBack(2);
-	intVector.EmplaceBack(2);
-	intVector.EmplaceBack(2);
-	intVector.EmplaceBack(2);
-	intVector.EmplaceBack(2);
-	PrintVector(intVector);
-	intVector.PopBack();
-	PrintVector(intVector);
-	intVector.Clear();
+	std::cout << "Range-based for loop:\n";
+	for (auto& value : values)
+		std::cout << value << std::endl;
+
+	std::cout << "Iterator:\n";
+	for (Vector<std::string>::Iterator it = values.begin();
+		it != values.end(); it++)
+	{
+		std::cout << *it << std::endl;
+	}
+
+	////Using Array.
+	//Array<std::string, 2> data;
+	//data[0] = "Kyle";
+	//data[1] = "Pearce";
+
+	///*for (size_t i = 0; i < data.Size(); i++)
+	//{
+	//	std::cout << data[i] << std::endl;
+	//}*/
+
+	//{
+	//	//Using Vector.
+	//	Vector<Vector3> vector;
+	//	vector.EmplaceBack(1.0f);
+	//	vector.EmplaceBack(2, 3, 4);
+	//	vector.EmplaceBack(1, 3, 4);
+	//	vector.EmplaceBack();
+	//	PrintVector(vector);
+	//	vector.PopBack();
+	//	vector.PopBack();
+	//	PrintVector(vector);
+	//	vector.EmplaceBack(5, 3, 0);
+	//	vector.EmplaceBack(1, 7, 9);
+	//	PrintVector(vector);
+
+	//	vector.Clear();
+	//	PrintVector(vector);
+	//	vector.EmplaceBack(5, 3, 0);
+	//	vector.EmplaceBack(1, 7, 9);
+	//	PrintVector(vector);
+	//}
+
+	//Vector<int> intVector;
+	//intVector.PushBack(5);
+	//intVector.EmplaceBack(2);
+	//intVector.EmplaceBack(2);
+	//intVector.EmplaceBack(2);
+	//intVector.EmplaceBack(2);
+	//intVector.EmplaceBack(2);
+	//PrintVector(intVector);
+	//intVector.PopBack();
+	//PrintVector(intVector);
+	//intVector.Clear();
 
 	std::cout << std::endl;
 	std::cout << "Press enter to close application..." << std::endl;
--- C++Engine/src/hpp/Vector.h
+++ C++Engine/src/hpp/Vector.h
@@ -1,8 +1,76 @@
 #pragma once
 
+template<typename Vector>
+class VectorIterator
+{
+public:
+	using ValueType = typename Vector::ValueType;
+	using PointerType = ValueType*;
+	using ReferenceType = ValueType&;
+public:
+	VectorIterator(PointerType ptr)
+		: m_Ptr(ptr) {}
+
+	VectorIterator& operator++()
+	{
+		m_Ptr++;
+		return *this;
+	}
+
+	VectorIterator operator++(int)
+	{
+		VectorIterator iterator = *this;
+		++(*this);
+		return iterator;
+	}
+
+	VectorIterator& operator--()
+	{
+		m_Ptr--;
+		return *this;
+	}
+
+	VectorIterator operator--(int)
+	{
+		VectorIterator iterator = *this;
+		--(*this);
+		return iterator;
+	}
+
+	ReferenceType operator[](int index)
+	{
+		return *(m_Ptr + index);
+	}
+
+	PointerType operator->()
+	{
+		return m_Ptr;
+	}
+
+	ReferenceType operator*()
+	{
+		return *m_Ptr;
+	}
+
+	bool operator==(const VectorIterator& other) const
+	{
+		return m_Ptr == other.m_Ptr;
+	}
+
+	bool operator!=(const VectorIterator& other) const
+	{
+		return !(*this == other);
+	}
+private:
+	PointerType m_Ptr;
+};
+
 template<typename T>
 class Vector
 {
+public:
+	using ValueType = T;
+	using Iterator = VectorIterator<Vector<T>>;
 public:
 	Vector()
 	{
@@ -84,6 +152,16 @@ public:
 	}
 
 	size_t Size() const { return m_Size; }
+
+	Iterator begin()
+	{
+		return Iterator(m_Data);
+	}
+
+	Iterator end()
+	{
+		return Iterator(m_Data + m_Size);
+	}
 private:
 	void ReAlloc(size_t newCapacity)
 	{
@@ -93,7 +171,7 @@ private:
 			m_Size = newCapacity;
 
 		for (size_t i = 0; i < m_Size; i++)
-			newBlock[i] = std::move(m_Data[i]);
+			new (&newBlock[i]) T(std::move(m_Data[i]));
 
 		for (size_t i = 0; i < m_Size; i++)
 			m_Data[i].~T();
